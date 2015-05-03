b6dd6dae984ddefa232e4c227e978aa0f4f14890
Colin Surprenant <colin.surprenant@gmail.com>
2015-05-02 21:37:48 -0400

    fix json serialization and cleanups
    initial specs for netflow codec
    
    bump version to 0.1.6
    
    cleanups
    
    remove unnecessary version check
    
    fixes elastic/logstash/issues#2945
    fixes elastic/logstash/issues#3108

--- lib/logstash/codecs/netflow.rb
+++ lib/logstash/codecs/netflow.rb
@@ -33,13 +33,16 @@ class LogStash::Codecs::Netflow < LogStash::Codecs::Base
   # See <https://github.com/logstash-plugins/logstash-codec-netflow/blob/master/lib/logstash/codecs/netflow/netflow.yaml> for the base set.
   config :definitions, :validate => :path
 
-  public
-  def initialize(params={})
+  NETFLOW5_FIELDS = ['version', 'flow_seq_num', 'engine_type', 'engine_id', 'sampling_algorithm', 'sampling_interval', 'flow_records']
+  NETFLOW9_FIELDS = ['version', 'flow_seq_num']
+  SWITCHED = /_switched$/
+  FLOWSET_ID = "flowset_id"
+
+  def initialize(params = {})
     super(params)
     @threadsafe = false
   end
 
-  public
   def register
     require "logstash/codecs/netflow/util"
     @templates = Vash.new()
@@ -64,7 +67,6 @@ class LogStash::Codecs::Netflow < LogStash::Codecs::Base
     end
   end # def register
 
-  public
   def decode(payload, &block)
     header = Header.read(payload)
 
@@ -75,169 +77,166 @@ class LogStash::Codecs::Netflow < LogStash::Codecs::Base
 
     if header.version == 5
       flowset = Netflow5PDU.read(payload)
+      flowset.records.each do |record|
+        yield(decode_netflow5(flowset, record))
+      end
     elsif header.version == 9
       flowset = Netflow9PDU.read(payload)
+      flowset.records.each do |record|
+        decode_netflow9(flowset, record).each{|event| yield(event)}
+      end
     else
       @logger.warn("Unsupported Netflow version v#{header.version}")
-      return
     end
-
-    flowset.records.each do |record|
-      if flowset.version == 5
-        event = LogStash::Event.new
-
-        # FIXME Probably not doing this right WRT JRuby?
-        #
-        # The flowset header gives us the UTC epoch seconds along with
-        # residual nanoseconds so we can set @timestamp to that easily
-        event.timestamp = LogStash::Timestamp.at(flowset.unix_sec, flowset.unix_nsec / 1000)
-        event[@target] = {}
-
-        # Copy some of the pertinent fields in the header to the event
-        ['version', 'flow_seq_num', 'engine_type', 'engine_id', 'sampling_algorithm', 'sampling_interval', 'flow_records'].each do |f|
-          event[@target][f] = flowset[f]
-        end
-
-        # Create fields in the event from each field in the flow record
-        record.each_pair do |k,v|
-          case k.to_s
-          when /_switched$/
-            # The flow record sets the first and last times to the device
-            # uptime in milliseconds. Given the actual uptime is provided
-            # in the flowset header along with the epoch seconds we can
-            # convert these into absolute times
-            millis = flowset.uptime - v
-            seconds = flowset.unix_sec - (millis / 1000)
-            micros = (flowset.unix_nsec / 1000) - (millis % 1000)
-            if micros < 0
-              seconds--
-              micros += 1000000
-            end
-            # FIXME Again, probably doing this wrong WRT JRuby?
-            event[@target][k.to_s] = Time.at(seconds, micros).utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
-          else
-            event[@target][k.to_s] = v
-          end
-        end
-
-        yield event
-      elsif flowset.version == 9
-        case record.flowset_id
-        when 0
-          # Template flowset
-          record.flowset_data.templates.each do |template|
-            catch (:field) do
-              fields = []
-              template.fields.each do |field|
-                entry = netflow_field_for(field.field_type, field.field_length)
-                if ! entry
-                  throw :field
-                end
-                fields += entry
-              end
-              # We get this far, we have a list of fields
-              #key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
-              key = "#{flowset.source_id}|#{template.template_id}"
-              @templates[key, @cache_ttl] = BinData::Struct.new(:endian => :big, :fields => fields)
-              # Purge any expired templates
-              @templates.cleanup!
-            end
-          end
-        when 1
-          # Options template flowset
-          record.flowset_data.templates.each do |template|
-            catch (:field) do
-              fields = []
-              template.option_fields.each do |field|
-                entry = netflow_field_for(field.field_type, field.field_length)
-                if ! entry
-                  throw :field
-                end
-                fields += entry
-              end
-              # We get this far, we have a list of fields
-              #key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
-              key = "#{flowset.source_id}|#{template.template_id}"
-              @templates[key, @cache_ttl] = BinData::Struct.new(:endian => :big, :fields => fields)
-              # Purge any expired templates
-              @templates.cleanup!
-            end
-          end
-        when 256..65535
-          # Data flowset
-          #key = "#{flowset.source_id}|#{event["source"]}|#{record.flowset_id}"
-          key = "#{flowset.source_id}|#{record.flowset_id}"
-          template = @templates[key]
-
-          if ! template
-            #@logger.warn("No matching template for flow id #{record.flowset_id} from #{event["source"]}")
-            @logger.warn("No matching template for flow id #{record.flowset_id}")
-            next
-          end
-
-          length = record.flowset_length - 4
-
-          # Template shouldn't be longer than the record and there should
-          # be at most 3 padding bytes
-          if template.num_bytes > length or ! (length % template.num_bytes).between?(0, 3)
-            @logger.warn("Template length doesn't fit cleanly into flowset", :template_id => record.flowset_id, :template_length => template.num_bytes, :record_length => length)
-            next
-          end
-
-          array = BinData::Array.new(:type => template, :initial_length => length / template.num_bytes)
-
-          records = array.read(record.flowset_data)
-
-          records.each do |r|
-            event = LogStash::Event.new(
-              LogStash::Event::TIMESTAMP => LogStash::Timestamp.at(flowset.unix_sec),
-              @target => {}
-            )
-
-            # Fewer fields in the v9 header
-            ['version', 'flow_seq_num'].each do |f|
-              event[@target][f] = flowset[f]
-            end
-
-            event[@target]['flowset_id'] = record.flowset_id
-
-            r.each_pair do |k,v|
-              case k.to_s
-              when /_switched$/
-                millis = flowset.uptime - v
-                seconds = flowset.unix_sec - (millis / 1000)
-                # v9 did away with the nanosecs field
-                micros = 1000000 - (millis % 1000)
-                event[@target][k.to_s] = Time.at(seconds, micros).utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
-              else
-                event[@target][k.to_s] = v
-              end
-            end
-
-            yield event
-          end
-        else
-          @logger.warn("Unsupported flowset id #{record.flowset_id}")
-        end
-      end
-    end
-  end # def filter
+  end
 
   private
+
+  def decode_netflow5(flowset, record)
+    event = {
+      LogStash::Event::TIMESTAMP => LogStash::Timestamp.at(flowset.unix_sec.snapshot, flowset.unix_nsec.snapshot / 1000),
+      @target => {}
+    }
+
+    # Copy some of the pertinent fields in the header to the event
+    NETFLOW5_FIELDS.each do |f|
+      event[@target][f] = flowset[f].snapshot
+    end
+
+    # Create fields in the event from each field in the flow record
+    record.each_pair do |k, v|
+      case k.to_s
+      when SWITCHED
+        # The flow record sets the first and last times to the device
+        # uptime in milliseconds. Given the actual uptime is provided
+        # in the flowset header along with the epoch seconds we can
+        # convert these into absolute times
+        millis = flowset.uptime - v
+        seconds = flowset.unix_sec - (millis / 1000)
+        micros = (flowset.unix_nsec / 1000) - (millis % 1000)
+        if micros < 0
+          seconds--
+          micros += 1000000
+        end
+        event[@target][k.to_s] = LogStash::Timestamp.at(seconds, micros).to_iso8601
+      else
+        event[@target][k.to_s] = v.snapshot
+      end
+    end
+
+    LogStash::Event.new(event)
+  end
+
+  def decode_netflow9(flowset, record)
+    events = []
+
+    case record.flowset_id
+    when 0
+      # Template flowset
+      record.flowset_data.templates.each do |template|
+        catch (:field) do
+          fields = []
+          template.fields.each do |field|
+            entry = netflow_field_for(field.field_type, field.field_length)
+            throw :field unless entry
+            fields += entry
+          end
+          # We get this far, we have a list of fields
+          #key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
+          key = "#{flowset.source_id}|#{template.template_id}"
+          @templates[key, @cache_ttl] = BinData::Struct.new(:endian => :big, :fields => fields)
+          # Purge any expired templates
+          @templates.cleanup!
+        end
+      end
+    when 1
+      # Options template flowset
+      record.flowset_data.templates.each do |template|
+        catch (:field) do
+          fields = []
+          template.option_fields.each do |field|
+            entry = netflow_field_for(field.field_type, field.field_length)
+            throw :field unless entry
+            fields += entry
+          end
+          # We get this far, we have a list of fields
+          #key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
+          key = "#{flowset.source_id}|#{template.template_id}"
+          @templates[key, @cache_ttl] = BinData::Struct.new(:endian => :big, :fields => fields)
+          # Purge any expired templates
+          @templates.cleanup!
+        end
+      end
+    when 256..65535
+      # Data flowset
+      #key = "#{flowset.source_id}|#{event["source"]}|#{record.flowset_id}"
+      key = "#{flowset.source_id}|#{record.flowset_id}"
+      template = @templates[key]
+
+      unless template
+        #@logger.warn("No matching template for flow id #{record.flowset_id} from #{event["source"]}")
+        @logger.warn("No matching template for flow id #{record.flowset_id}")
+        next
+      end
+
+      length = record.flowset_length - 4
+
+      # Template shouldn't be longer than the record and there should
+      # be at most 3 padding bytes
+      if template.num_bytes > length or ! (length % template.num_bytes).between?(0, 3)
+        @logger.warn("Template length doesn't fit cleanly into flowset", :template_id => record.flowset_id, :template_length => template.num_bytes, :record_length => length)
+        next
+      end
+
+      array = BinData::Array.new(:type => template, :initial_length => length / template.num_bytes)
+      records = array.read(record.flowset_data)
+
+      records.each do |r|
+        event = {
+          LogStash::Event::TIMESTAMP => LogStash::Timestamp.at(flowset.unix_sec),
+          @target => {}
+        }
+
+        # Fewer fields in the v9 header
+        NETFLOW9_FIELDS.each do |f|
+          event[@target][f] = flowset[f].snapshot
+        end
+
+        event[@target][FLOWSET_ID] = record.flowset_id.snapshot
+
+        r.each_pair do |k, v|
+          case k.to_s
+          when SWITCHED
+            millis = flowset.uptime - v
+            seconds = flowset.unix_sec - (millis / 1000)
+            # v9 did away with the nanosecs field
+            micros = 1000000 - (millis % 1000)
+            event[@target][k.to_s] = LogStash::Timestamp.at(seconds, micros).to_iso8601
+          else
+            event[@target][k.to_s] = v.snapshot
+          end
+        end
+
+        events << LogStash::Event.new(event)
+      end
+    else
+      @logger.warn("Unsupported flowset id #{record.flowset_id}")
+    end
+
+    events
+  end
+
   def uint_field(length, default)
     # If length is 4, return :uint32, etc. and use default if length is 0
     ("uint" + (((length > 0) ? length : default) * 8).to_s).to_sym
   end # def uint_field
 
-  private
   def netflow_field_for(type, length)
     if @fields.include?(type)
       field = @fields[type]
       if field.is_a?(Array)
 
-        if field[0].is_a?(Integer)
-          field[0] = uint_field(length, field[0])
-        end
+        field[0] = uint_field(length, field[0]) if field[0].is_a?(Integer)
 
         # Small bit of fixup for skip or string field types where the length
         # is dynamic
@@ -248,7 +247,8 @@ class LogStash::Codecs::Netflow < LogStash::Codecs::Base
           field += [{:length => length, :trim_padding => true}]
         end
 
-        @logger.debug("Definition complete", :field => field)
+        @logger.debug? and @logger.debug("Definition complete", :field => field)
+
         [field]
       else
         @logger.warn("Definition should be an array", :field => field)
--- spec/codecs/netflow_spec.rb
+++ spec/codecs/netflow_spec.rb
@@ -1,1 +1,208 @@
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/codecs/netflow"
+
+describe LogStash::Codecs::Netflow do
+  subject do
+    LogStash::Codecs::Netflow.new.tap do |codec|
+      expect{codec.register}.not_to raise_error
+    end
+  end
+
+  let(:decode) do
+    [].tap do |events|
+      subject.decode(data){|event| events << event}
+    end
+  end
+
+  context "Netflow 5" do
+    let(:data) do
+      # this netflow raw data was produced with softflowd and captured with netcat
+      # softflowd -D -i eth0 -v 5 -t maxlife=1 -n 127.0.01:8765
+      # nc -k -4 -u -l 127.0.0.1 8765 > netflow5.dat
+      IO.read(File.join(File.dirname(__FILE__), "netflow5.dat"), :mode => "rb")
+    end
+
+    let(:json_events) do
+      events = []
+      events << <<-END
+        {
+          "@timestamp": "2015-05-02T18:38:08.280Z",
+          "netflow": {
+            "version": 5,
+            "flow_seq_num": 0,
+            "engine_type": 0,
+            "engine_id": 0,
+            "sampling_algorithm": 0,
+            "sampling_interval": 0,
+            "flow_records": 2,
+            "ipv4_src_addr": "10.0.2.2",
+            "ipv4_dst_addr": "10.0.2.15",
+            "ipv4_next_hop": "0.0.0.0",
+            "input_snmp": 0,
+            "output_snmp": 0,
+            "in_pkts": 5,
+            "in_bytes": 230,
+            "first_switched": "2015-06-21T11:40:52.280Z",
+            "last_switched": "2015-05-02T18:38:08.279Z",
+            "l4_src_port": 54435,
+            "l4_dst_port": 22,
+            "tcp_flags": 16,
+            "protocol": 6,
+            "src_tos": 0,
+            "src_as": 0,
+            "dst_as": 0,
+            "src_mask": 0,
+            "dst_mask": 0
+          },
+          "@version": "1"
+        }
+      END
+
+      events << <<-END
+        {
+          "@timestamp": "2015-05-02T18:38:08.280Z",
+          "netflow": {
+            "version": 5,
+            "flow_seq_num": 0,
+            "engine_type": 0,
+            "engine_id": 0,
+            "sampling_algorithm": 0,
+            "sampling_interval": 0,
+            "flow_records": 2,
+            "ipv4_src_addr": "10.0.2.15",
+            "ipv4_dst_addr": "10.0.2.2",
+            "ipv4_next_hop": "0.0.0.0",
+            "input_snmp": 0,
+            "output_snmp": 0,
+            "in_pkts": 4,
+            "in_bytes": 304,
+            "first_switched": "2015-06-21T11:40:52.280Z",
+            "last_switched": "2015-05-02T18:38:08.279Z",
+            "l4_src_port": 22,
+            "l4_dst_port": 54435,
+            "tcp_flags": 24,
+            "protocol": 6,
+            "src_tos": 0,
+            "src_as": 0,
+            "dst_as": 0,
+            "src_mask": 0,
+            "dst_mask": 0
+          },
+          "@version": "1"
+        }
+      END
+
+      events.map{|event| event.gsub(/\s+/, "")}
+    end
+
+    it "should decode raw data" do
+      expect(decode.size).to eq(2)
+
+      expect(decode[0]["[netflow][version]"]).to eq(5)
+      expect(decode[0]["[netflow][ipv4_src_addr]"]).to eq("10.0.2.2")
+      expect(decode[0]["[netflow][ipv4_dst_addr]"]).to eq("10.0.2.15")
+      expect(decode[0]["[netflow][l4_src_port]"]).to eq(54435)
+      expect(decode[0]["[netflow][l4_dst_port]"]).to eq(22)
+      expect(decode[0]["[netflow][tcp_flags]"]).to eq(16)
+
+      expect(decode[1]["[netflow][version]"]).to eq(5)
+      expect(decode[1]["[netflow][ipv4_src_addr]"]).to eq("10.0.2.15")
+      expect(decode[1]["[netflow][ipv4_dst_addr]"]).to eq("10.0.2.2")
+      expect(decode[1]["[netflow][l4_src_port]"]).to eq(22)
+      expect(decode[1]["[netflow][l4_dst_port]"]).to eq(54435)
+      expect(decode[1]["[netflow][tcp_flags]"]).to eq(24)
+    end
+
+    it "should serialize to json" do
+      expect(decode[0].to_json).to eq(json_events[0])
+      expect(decode[1].to_json).to eq(json_events[1])
+    end
+  end
+
+  context "Netflow 9" do
+    let(:data) do
+      # this netflow raw data was produced with softflowd and captured with netcat
+      # softflowd -D -i eth0 -v 9 -t maxlife=1 -n 127.0.01:8765
+      # nc -k -4 -u -l 127.0.0.1 8765 > netflow9.dat
+      IO.read(File.join(File.dirname(__FILE__), "netflow9.dat"), :mode => "rb")
+    end
+
+    let(:json_events) do
+      events = []
+      events << <<-END
+        {
+          "@timestamp": "2015-05-02T22:10:07.000Z",
+          "netflow": {
+            "version": 9,
+            "flow_seq_num": 0,
+            "flowset_id": 1024,
+            "ipv4_src_addr": "10.0.2.2",
+            "ipv4_dst_addr": "10.0.2.15",
+            "last_switched": "2015-05-02T22:10:07.999Z",
+            "first_switched": "2015-06-21T15:12:49.999Z",
+            "in_bytes": 230,
+            "in_pkts": 5,
+            "input_snmp": 0,
+            "output_snmp": 0,
+            "l4_src_port": 57369,
+            "l4_dst_port": 22,
+            "protocol": 6,
+            "tcp_flags": 16,
+            "ip_protocol_version": 4
+          },
+          "@version": "1"
+        }
+      END
+
+      events << <<-END
+        {
+          "@timestamp": "2015-05-02T22:10:07.000Z",
+          "netflow": {
+            "version": 9,
+            "flow_seq_num": 0,
+            "flowset_id": 1024,
+            "ipv4_src_addr": "10.0.2.15",
+            "ipv4_dst_addr": "10.0.2.2",
+            "last_switched": "2015-05-02T22:10:07.999Z",
+            "first_switched": "2015-06-21T15:12:49.999Z",
+            "in_bytes": 352,
+            "in_pkts": 4,
+            "input_snmp": 0,
+            "output_snmp": 0,
+            "l4_src_port": 22,
+            "l4_dst_port": 57369,
+            "protocol": 6,
+            "tcp_flags": 24,
+            "ip_protocol_version": 4
+          },
+          "@version": "1"
+        }
+      END
+
+      events.map{|event| event.gsub(/\s+/, "")}
+    end
+
+    it "should decode raw data" do
+      expect(decode.size).to eq(2)
+
+      expect(decode[0]["[netflow][version]"]).to eq(9)
+      expect(decode[0]["[netflow][ipv4_src_addr]"]).to eq("10.0.2.2")
+      expect(decode[0]["[netflow][ipv4_dst_addr]"]).to eq("10.0.2.15")
+      expect(decode[0]["[netflow][l4_src_port]"]).to eq(57369)
+      expect(decode[0]["[netflow][l4_dst_port]"]).to eq(22)
+      expect(decode[0]["[netflow][tcp_flags]"]).to eq(16)
+
+      expect(decode[1]["[netflow][version]"]).to eq(9)
+      expect(decode[1]["[netflow][ipv4_src_addr]"]).to eq("10.0.2.15")
+      expect(decode[1]["[netflow][ipv4_dst_addr]"]).to eq("10.0.2.2")
+      expect(decode[1]["[netflow][l4_src_port]"]).to eq(22)
+      expect(decode[1]["[netflow][l4_dst_port]"]).to eq(57369)
+      expect(decode[1]["[netflow][tcp_flags]"]).to eq(24)
+    end
+
+    it "should serialize to json" do
+      expect(decode[0].to_json).to eq(json_events[0])
+      expect(decode[1].to_json).to eq(json_events[1])
+    end
+  end
+end
